0f49050c8f9a32825cea5f5c207a9223f115890d
Vinicius <viniciusflores379@gmail.com>
2020-04-18 14:55:12 -0300

    :tada: Initial cypress basic

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+describe('Cypress basic', () => {
+  it.only('Should visit a page and assert title', () => {
+    cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+    cy.pause();
+    
+    // cy.title().should('be.equal', 'Campo de Treinamento')
+    // cy.title().should('contain', 'Campo').debug()
+    cy.title()
+      .should('be.equal', 'Campo de Treinamento')
+      .should('contain', 'Campo')
+
+
+    //TODO: imprimir title no console
+    //TODO: escrever o title em algum campo
+  })
+
+  it('Should find and interact with an element', () => {
+    cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+    cy.get('#buttonSimple')
+      .click()
+      .should('have.value', 'Obrigado!')
+
+
+
+  })
+
+})
