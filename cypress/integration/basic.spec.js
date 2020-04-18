9f231b8ef426926e8183c53e67a0e8360dc205bb
Vinicius <viniciusflores379@gmail.com>
2020-04-18 15:30:10 -0300

    :sparkles: add link test

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -24,8 +24,6 @@ describe('Cypress basic', () => {
       .click()
       .should('have.value', 'Obrigado!')
 
-
-
   })
 
 })
---
+++ cypress/integration/elements.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress"/>
+
+describe('Work with basic elements', () => {
+  before(()=>{
+    cy.visit('https://wcaquino.me/cypress/componentes.html')
+  })
+
+  beforeEach(()=> {
+    cy.reload()
+  })
+
+  it('Text', () => {
+    cy.get('body').should('contain', 'Cuidado')
+    cy.get('span').should('contain', 'Cuidado')
+    cy.get('.facilAchar').should('contain', 'Cuidado')
+    cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
+  })
+
+  it('Links', () => {
+    cy.contains('Voltar').click()
+    cy.get('#resultado').should('have.text', 'Voltou!')
+    cy.reload()
+    cy.get('#resultado').should('have.not.text', 'Voltou!')
+    cy.contains('Voltar').click()
+    cy.get('#resultado').should('have.text', 'Voltou!')
+  })
+})
