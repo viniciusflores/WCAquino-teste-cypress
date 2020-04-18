eeedc4a14cd06977e1a0f6c3b6858480979203e0
Vinicius <viniciusflores379@gmail.com>
2020-04-18 19:54:18 -0300

    :sparkles: cypress invoke

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -20,7 +20,28 @@ describe('Helpers...', ()=>{
     cy.get('#buttonSimple').then(()=>console.log('First button'))
     cy.wrap(promisse).then(num => console.log(num))
     cy.get('#buttonList').then(()=>console.log('Second button'))
+  })
 
-    
+  it('Its',() => {
+    const obj = {nome: 'User', idade: 20}
+    expect(obj).to.have.property('nome')
+    cy.wrap(obj).should('have.property','nome', 'User')
+    cy.wrap(obj).its('nome').should('be.equal', 'User')
+
+    const obj2 = {nome: 'User', idade: 20, endereco: {rua: 'dos bobos', numero: 0}}
+    cy.wrap(obj2).its('endereco').should('have.property','rua')
+    cy.wrap(obj2).its('endereco').its('rua').should('contain','bobos')
+    cy.wrap(obj2).its('endereco.rua').should('contain','bobos')
+  })
+
+  it.only('Invoke', () =>{
+    const getValue = () => 1;
+    const soma = (a,b) => a + b;
+
+    cy.wrap({fn: getValue}).invoke('fn').should('be.equal',1)
+    cy.wrap({fn: soma}).invoke('fn',2,5).should('be.equal',7)
+
+    cy.visit('https://wcaquino.me/cypress/componentes.html')
+    cy.get('#formNome').invoke('val','Texto via invoke')
   })
 })
