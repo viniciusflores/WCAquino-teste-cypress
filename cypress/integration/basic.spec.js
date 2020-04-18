a314ea392958acf4b99db110b5e3ee415cb49f63
Vinicius <viniciusflores379@gmail.com>
2020-04-18 18:28:11 -0300

    :sparkles: add then test

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,10 +1,10 @@
 /// <reference types="cypress" />
 
 describe('Cypress basic', () => {
-  it.only('Should visit a page and assert title', () => {
+  it('Should visit a page and assert title', () => {
     cy.visit('https://wcaquino.me/cypress/componentes.html')
 
-    cy.pause();
+    //cy.pause();
     
     // cy.title().should('be.equal', 'Campo de Treinamento')
     // cy.title().should('contain', 'Campo').debug()
@@ -13,8 +13,10 @@ describe('Cypress basic', () => {
       .should('contain', 'Campo')
 
 
-    //TODO: imprimir title no console
-    //TODO: escrever o title em algum campo
+    cy.title().then(title => {
+      console.log(title)
+    })
+  
   })
 
   it('Should find and interact with an element', () => {
--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -15,10 +15,42 @@ describe('Waiters...', () => {
     cy.get('#novoCampo').type('funcionou')
   })
 
+  it('Should do retrys', () => {
+    cy.get('#novoCampo').should('not.exist')
+    cy.get('#buttonDelay').click()
+    cy.get('#novoCampo').should('not.exist')
+    cy.get('#novoCampo')
+      .should('exist')
+      .type('funciona')
+  })
 
 
+  it('Should use find', () => {
+    cy.get('#buttonList').click()
+    cy.get('#lista li')
+      .find('span')
+      .should('contain', 'Item 1')
+    cy.get('#lista li')
+      //.find('span')
+      .should('contain', 'Item 2')
+  })
 
+  it('Should use timeout', () =>{
+    cy.get('#buttonDelay').click()
+    //cy.get('#novoCampo', {timeout: 1000}).should('exist')
+    cy.get('#novoCampo').should('exist')
+  })  
 
-
+  it.only('Should vs Then', ()=> {
+    cy.get('#buttonListDOM').click()
+    // cy.get('#lista li span').should($el => {
+    //   console.log($el)
+    //   expect($el).to.have.length(1)
+    // })
+    cy.get('#lista li span').then($el => {
+      console.log($el)
+      expect($el).to.have.length(1)
+    })
+  })
 
 })
