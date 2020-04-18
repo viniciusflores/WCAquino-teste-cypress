90223f316b973d226ebe011a28b1df7e1a48600c
Vinicius <viniciusflores379@gmail.com>
2020-04-18 20:07:11 -0300

    :sparkles: cypress invoke

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -41,7 +41,11 @@ describe('Helpers...', ()=>{
     cy.wrap({fn: getValue}).invoke('fn').should('be.equal',1)
     cy.wrap({fn: soma}).invoke('fn',2,5).should('be.equal',7)
 
-    cy.visit('https://wcaquino.me/cypress/componentes.html')
+     cy.visit('https://wcaquino.me/cypress/componentes.html')
     cy.get('#formNome').invoke('val','Texto via invoke')
+
+    cy.window().invoke('alert', 'oh my god, bing')
+    cy.get('#resultado')
+      .invoke('html', '<input type="button", value="hacked!"/>')
   })
 })
